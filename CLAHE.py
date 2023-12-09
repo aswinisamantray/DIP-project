c2ebe82d3462b1c6867a9117b405f97f5820d2c0
Aswini Samantray <aswinisamantray.7149@gmail.com>
2023-12-09 13:00:42 +0530

    Create CLAHE.py
    CLAHE

---
+++ CLAHE.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_hist(histogram,title):
+    plt.bar(np.arange(256), histogram, width=1, color='gray')
+    plt.title(title)
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
+    plt.show()
+    
+def get_histogram(img):
+    img = np.clip(img, 0, 255).astype(int)
+    histogram = np.zeros(256)
+    # loop through pixels and sum up counts of pixels
+    for pixel in img:
+        histogram[pixel] += 1
+    # return our final result
+    return histogram
+
+def dynamic_histogram_equalization(img, clip_limit=3.0, grid_size=(8, 8)):
+    lab_img = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+    l_channel, a_channel, b_channel = cv.split(lab_img)
+
+    clahe = cv.createCLAHE(clipLimit=clip_limit, tileGridSize=grid_size)
+    cl_channel = clahe.apply(l_channel)
+
+    enhanced_lab = cv.merge([cl_channel, a_channel, b_channel])
+    enhanced_img = cv.cvtColor(enhanced_lab, cv.COLOR_LAB2RGB)
+
+    return enhanced_img
+
+input_image = cv.imread('DIP_image.jpg')
+enhanced_image = dynamic_histogram_equalization(input_image)
+
+plot_hist(get_histogram(input_image),"Input image histogram")
+plot_hist(get_histogram(enhanced_image),"Output image histogram")
+
+M_input_image=np.mean(input_image)
+M_enhanced_image=np.mean(enhanced_image)
+print(M_enhanced_image-M_input_image)
+
+plt.imshow(enhanced_image)
+plt.title('Enhanced Image')
+plt.axis('off')
+
+plt.show()
